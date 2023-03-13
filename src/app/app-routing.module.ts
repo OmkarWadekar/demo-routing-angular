3545fd9e76d172805f0e344883104f39c87bc252
OmkarWadekar <wadekaromkar08@gmail.com>
2023-03-13 19:14:40 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+
+//each route is an object with the path
+const routes: Routes = [
+  {path:'login',component: LoginComponent},
+  {path:'home',component: HomeComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar', //custom component name to be used in other componentss
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+
+}
